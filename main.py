a1c4d7274fda1e7c1c724ae610fa0712685ab60c
danielvh8 <66780852+danielvh8@users.noreply.github.com>
2021-12-20 15:29:06 +0100

    ....

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+import sklearn
+
+import matplotlib.pyplot as plt
+import math
+import seaborn as sns
+import sklearn
+import itertools
+from sklearn.model_selection import train_test_split
+
+trainData = pd.read_csv("/Data/train.csv")
+testData = pd.read_csv("/Data/test.csv")
+
+trainData.head()
+
+#data = censusData.drop(columns="class")
+#target = censusData["class"]
+
+# X_train, X_test, y_train, y_test = train_test_split()
+
+
+
+
